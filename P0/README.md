09acd88c9938544aeb41e2ec7ccbb0e13777e0c8
Vinicius Mioto <vm20@inf.ufpr.br>
2023-03-22 16:36:35 -0300

    [env] update init and make files

---
+++ P0/README.md
@@ -0,0 +1,3 @@
+# Project 0: QUEUE LIBRARY
+
+* Description: https://wiki.inf.ufpr.br/maziero/doku.php?id=so:biblioteca_de_filas
--- P0/queue.c
+++ P0/queue.c
@@ -1,1 +1,18 @@
 // GRR20203931 - Vinícius Mioto
+# include "queue.h"
+
+int queue_size (queue_t *queue) {
+    return 0;
+}
+
+void queue_print (char *name, queue_t *queue, void print_elem (void*) ) {
+    return;
+}
+
+int queue_append (queue_t **queue, queue_t *elem) {
+    return 0;
+}
+
+int queue_remove (queue_t **queue, queue_t *elem) {
+    return 0;
+}
